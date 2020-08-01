5ecba674bea0566ec4902ef37bfab7ba5a163e44
JaDogg <JaDogg@users.noreply.github.com>
2020-08-01 22:07:01 +0100

    Fish shell configs
    Signed-off-by: JaDogg <JaDogg@users.noreply.github.com>

---
+++ config.fish
@@ -0,0 +1,42 @@
+function fish_prompt
+    powerline-shell --shell bare $status
+end
+
+function hybrid_bindings --description "Vi-style bindings that inherit emacs-style bindings in all modes"
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
+end
+set -g fish_key_bindings hybrid_bindings
+
+# Emulates vim's cursor shape behavior
+# Set the normal and visual mode cursors to a block
+set fish_cursor_default block
+# Set the insert mode cursor to a line
+set fish_cursor_insert line
+# Set the replace mode cursor to an underscore
+set fish_cursor_replace_one underscore
+# The following variable can be used to configure cursor shape in
+# visual mode, but due to fish_cursor_default, is redundant here
+set fish_cursor_visual block
+
+function fish_mode_prompt
+end
+
+alias import_fishrc='source ~/.config/fish/config.fish'
+alias edit_fishrc='vi ~/.config/fish/config.fish'
+alias l='ls -ltrah'
+alias c='clear; ls -ltrah'
+alias rm='rm -i'
+alias act='. .venv/bin/activate.fish'
+alias pg='ping google.com'
+
+set JAVA_HOME /usr/lib/jvm/default-java
+set  M2_HOME /home/pi/maven
+
+set -Ua fish_user_paths /usr/local/go/bin
+set -Ua fish_user_paths /home/pi/scripts
+set -Ua fish_user_paths /home/pi/.local/bin
+set -Ua fish_user_paths /home/pi/maven/bin
+
--- copy_latest.sh
+++ copy_latest.sh
@@ -2,6 +2,7 @@
 
 cp ~/.vimrc .
 cp ~/.bashrc .bashrc
+cp ~/.config/fish/config.fish .
 echo "Used plugins" > plugins.txt
 echo "=============" >> plugins.txt
 ls ~/.vim/pack/vendor/start >> plugins.txt
